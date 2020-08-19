a52615a2a5dc94eb9b0d1a59e2df9b292435cb42
Akash Agarwal <akash191095@gmail.com>
2020-08-19 15:11:44 +0530

    initialize system with 1 server

--- src/pages/System.js
+++ src/pages/System.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { useContext, useEffect } from "react";
+import Server from "../components/Server";
+import { StoreContext } from "../contexts/Store";
 
 function System() {
-    return <p>Hello from the system</p>
+  const { state, dispatch } = useContext(StoreContext);
+  const { servers } = state;
+
+  // Initialize the system
+  useEffect(() => {
+    dispatch({ type: "add_server", payload: { id: 1 } });
+  }, []);
+
+  return (
+    <main>
+      {servers.map(({ id }) => (
+        <Server id={id} key={id} />
+      ))}
+    </main>
+  );
 }
 
 export default System;
