846c07bbd19d34e20d787ed2cd29c494a468c2f1
Akash Agarwal <akash191095@gmail.com>
2020-08-26 12:54:18 +0530

    add more tests

--- src/__tests__/System.test.js
+++ src/__tests__/System.test.js
@@ -15,7 +15,7 @@ test("Add a new task", async () => {
   expect(screen.getByText("00.0")).toBeInTheDocument();
 });
 
-test("System only processes 1 task at a time", async () => {
+test("Server only processes 1 task at a time", async () => {
   const addTaskText = /add tasks/i;
   render(
     <StoreProvider>
@@ -24,5 +24,75 @@ test("System only processes 1 task at a time", async () => {
   );
   fireEvent.click(screen.getByText(addTaskText));
   fireEvent.click(screen.getByText(addTaskText));
-  expect(screen.getByText("waiting")).toBeInTheDocument();
+  fireEvent.click(screen.getByText(addTaskText));
+
+  expect(screen.getAllByText("waiting").length).toBe(2);
+});
+
+test("Add tasks", () => {
+  const addTaskText = /add tasks/i;
+  const numberOfTasksToAdd = 50;
+  const totalTaskText = new RegExp(`tasks: #${numberOfTasksToAdd}`, "i");
+
+  render(
+    <StoreProvider>
+      <System />
+    </StoreProvider>
+  );
+
+  for (let i = 0; i < numberOfTasksToAdd; i++) {
+    fireEvent.click(screen.getByText(addTaskText));
+  }
+  expect(screen.getByText(totalTaskText)).toBeInTheDocument();
+});
+
+test("Remove tasks", () => {
+  const addTaskText = /add tasks/i;
+  const numberOfTasksToAdd = 50;
+  const numberOfTasksToRemove = 15;
+  const totalTaskText = new RegExp(
+    `tasks: #${numberOfTasksToAdd - numberOfTasksToRemove}`,
+    "i"
+  );
+
+  render(
+    <StoreProvider>
+      <System />
+    </StoreProvider>
+  );
+
+  // add tasks
+  for (let i = 0; i < numberOfTasksToAdd; i++) {
+    fireEvent.click(screen.getByText(addTaskText));
+  }
+
+  // remove tasks
+  for (let i = 0; i < numberOfTasksToRemove; i++) {
+    fireEvent.click(screen.getAllByAltText("delete")[0]);
+  }
+
+  expect(screen.getByText(totalTaskText)).toBeInTheDocument();
+});
+
+test("Add Server", () => {
+  const addServerButton = /add server/i;
+  const initialServersRunning = 1;
+  const numberOfServersToAdd = 5;
+  const serversRunning = new RegExp(
+    `servers running: #${numberOfServersToAdd + initialServersRunning}`,
+    "i"
+  );
+
+  render(
+    <StoreProvider>
+      <System />
+    </StoreProvider>
+  );
+
+  // add servers
+  for (let i = 0; i < numberOfServersToAdd; i++) {
+    fireEvent.click(screen.getByText(addServerButton));
+  }
+
+  expect(screen.getByText(serversRunning)).toBeInTheDocument();
 });
