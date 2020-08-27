9467e8f606ac41b7251aa131aa6e097ee4e0e2d6
Akash Agarwal <akash@pop-os.localdomain>
2020-08-27 12:34:33 +0530

    add pagination

--- src/components/TaskQueue.js
+++ src/components/TaskQueue.js
@@ -1,4 +1,5 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
+import ReactPaginate from "react-paginate";
 import { StoreContext } from "../contexts/Store";
 import Task from "./Task";
 
@@ -6,14 +7,43 @@ function TaskQueue() {
   const {
     data: { taskQueue },
   } = useContext(StoreContext);
+  const ITEMS_PER_PAGE = 20;
+  const pageCount = Math.ceil(taskQueue.length / ITEMS_PER_PAGE);
+  const [offset, setOffset] = useState(0);
+
+  function handlePageChange(data) {
+    const selected = data.selected;
+    const offset = Math.ceil(selected * ITEMS_PER_PAGE);
+    setOffset(offset);
+  }
+
+  function getTasks(array, offset) {
+    return array.slice(offset, offset + ITEMS_PER_PAGE);
+  }
 
   return (
     <div className="tasks-container">
-      {taskQueue.map(({ id, running }) => (
-        <article key={id}>
-          <Task running={running} id={id} />
-        </article>
-      ))}
+      {getTasks(taskQueue, offset).length >= 1 &&
+        getTasks(taskQueue, offset).map(({ id, running }) => (
+          <article key={id}>
+            <Task running={running} id={id} />
+          </article>
+        ))}
+      <div className="pagination">
+        <ReactPaginate
+          previousLabel="previous"
+          nextLabel="next"
+          breakLabel="..."
+          breakClassName="break-me"
+          pageCount={pageCount}
+          marginPagesDisplayed={2}
+          pageRangeDisplayed={5}
+          onPageChange={handlePageChange}
+          containerClassName="pagination"
+          subContainerClassName="pages pagination"
+          activeClassName="active"
+        />
+      </div>
     </div>
   );
 }
